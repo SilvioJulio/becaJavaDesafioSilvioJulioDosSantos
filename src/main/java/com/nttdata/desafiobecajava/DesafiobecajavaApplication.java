71ee5ce3971f090614e8e9442677c3ef469ce5fa
Familia_Sal <sivicr3@hotmail.com>
2022-01-19 23:04:32 -0300

    Adicinanado novos arquivos iniciais do projetos

---
+++ src/main/java/com/nttdata/desafiobecajava/DesafiobecajavaApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.desafiobecajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobecajavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobecajavaApplication.class, args);
+	}
+
+}
