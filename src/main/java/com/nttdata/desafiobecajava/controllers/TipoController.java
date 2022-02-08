708c0f8d2979f9c1828eba8b6184d818b84a3a78
Familia_Sal <sivicr3@hotmail.com>
2022-02-08 14:24:05 -0300

    Foi adicionado um condicional, que verifica o campo se está null ou vazio.

--- src/main/java/com/nttdata/desafiobecajava/controllers/TipoController.java
+++ src/main/java/com/nttdata/desafiobecajava/controllers/TipoController.java
@@ -1,8 +1,15 @@
 package com.nttdata.desafiobecajava.controllers;
 
+<<<<<<< desafio6
 import com.nttdata.desafiobecajava.dtos.requests.PostTipoDtoRequest;
 import com.nttdata.desafiobecajava.dtos.responses.GetTipoResponse;
 import com.nttdata.desafiobecajava.dtos.responses.PostTipoDtoResponse;
+=======
+import com.nttdata.desafiobecajava.domains.Tipo;
+import com.nttdata.desafiobecajava.dtos.requests.TipoRequest;
+import com.nttdata.desafiobecajava.dtos.responses.TipoResponse;
+import com.nttdata.desafiobecajava.mappers.MapperTipoToTipoResponse;
+>>>>>>> local
 import com.nttdata.desafiobecajava.services.TipoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +25,7 @@ public class TipoController {
     private TipoService tipoService;
 
     @PostMapping
+<<<<<<< desafio6
     public ResponseEntity<PostTipoDtoResponse> criar(@RequestBody PostTipoDtoRequest postTipoDtoRequest) {
 
         PostTipoDtoResponse tipoCriado = tipoService.criar(postTipoDtoRequest);
@@ -25,19 +33,40 @@ public class TipoController {
         System.out.println("Tipo adicionado ao sistema com sucesso!");
 
        return ResponseEntity.created(null).body(tipoCriado);
+=======
+    public ResponseEntity<TipoResponse> criar(@RequestBody TipoRequest tipoRequest) {
+         if(tipoRequest.getDescricao().equals(null)||tipoRequest.getDescricao().equals("")) {
+             throw new RuntimeException("O campo desscrição não pode está vazio ou ausente !");
+         }
+        TipoResponse tipoResponse = tipoService.criar(tipoRequest);
+
+        System.out.println("Tipo adicionado ao sistema com sucesso!");
+
+        return ResponseEntity.created(null).body(tipoResponse);
+>>>>>>> local
 
     }
 
     @PatchMapping("/{id}")
+<<<<<<< desafio6
     public ResponseEntity<PostTipoDtoResponse> editar(@RequestBody PostTipoDtoRequest postTipoDtoRequest,@PathVariable Long id) {
 
         PostTipoDtoResponse editar = tipoService.editar(postTipoDtoRequest,id);
+=======
+    public ResponseEntity<TipoResponse> editar(@RequestBody TipoRequest tipoRequest, @PathVariable Long id) {
+
+        TipoResponse editar = tipoService.editar(tipoRequest, id);
+>>>>>>> local
 
         return ResponseEntity.ok(editar);
     }
 
     @GetMapping("/{id}")
+<<<<<<< desafio6
     public ResponseEntity<GetTipoResponse> obter(@PathVariable Long id) {
+=======
+    public ResponseEntity<TipoResponse> obter(@PathVariable Long id) {
+>>>>>>> local
 
         GetTipoResponse obterTipo = tipoService.obter(id);
 
